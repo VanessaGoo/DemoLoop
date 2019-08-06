3c7dc7384c20edf34f3e7b9ea1488ee67ee0a93f
17010336 <17010336@myrp.edu.sg>
2019-08-06 16:42:54 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/demoloop/MainActivity.kt
@@ -0,0 +1,22 @@
+package sg.edu.rp.c346.demoloop
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button2.setOnClickListener {
+            val word = editText.text.toString()
+
+            for(letter in word) {
+                Log.d("MainActivity", "$letter")
+            }
+        }
+    }
+}
